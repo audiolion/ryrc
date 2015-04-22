f6c2ffed36b3c453693f9c465ef4259d0c0c2a16
Ryan Castner <castner.rr@gmail.com>
2015-04-22 13:26:33 -0400

    initial commit

---
+++ Data/Units.hs
@@ -0,0 +1,195 @@
+-- | This module provides definitions for Measurement System Units and ways to parse
+-- text into data so that it can be converted and then convert it back to Strings for
+-- output. The Ry-RC bot uses this module to do the heavy lifting for unit conversions
+module Data.Units where
+
+import Data.List
+import Data.Char
+import Data.Maybe
+import Control.Applicative
+
+type Prefix = String
+type Power  = String
+
+type PrefixError = String
+
+-- | The definitions for this list of String are taken from the
+-- Pint unit conversion library for the Python programming language.
+-- Check them out on <https://github.com/hgrecco/pint GitHub>.
+definitions :: [String]
+definitions = 
+  ["yocto- = 1e-24 = y-"
+  ,"zepto- = 1e-21 = z-"
+  ,"atto- =  1e-18 = a-"
+  ,"femto- = 1e-15 = f-"
+  ,"pico- =  1e-12 = p-"
+  ,"nano- =  1e-9  = n-"
+  ,"micro- = 1e-6  = u-"
+  ,"milli- = 1e-3  = m-"
+  ,"centi- = 1e-2  = c-"
+  ,"deci- =  1e-1  = d-"
+  ,"deca- =  1e+1  = da-"
+  ,"hecto- = 1e2   = h-"
+  ,"kilo- =  1e3   = k-"
+  ,"mega- =  1e6   = M-"
+  ,"giga- =  1e9   = G-"
+  ,"tera- =  1e12  = T-"
+  ,"peta- =  1e15  = P-"
+  ,"exa- =   1e18  = E-"
+  ,"zetta- = 1e21  = Z-"
+  ,"yotta- = 1e24  = Y-"
+
+    -- binary_prefixes
+  ,"kibi- = 2**10 = Ki-"
+  ,"mebi- = 2**20 = Mi-"
+  ,"gibi- = 2**30 = Gi-"
+  ,"tebi- = 2**40 = Ti-"
+  ,"pebi- = 2**50 = Pi-"
+  ,"exbi- = 2**60 = Ei-"
+  ,"zebi- = 2**70 = Zi-"
+  ,"yobi- = 2**80 = Yi-"
+
+    -- base_prefixes
+  ,"met-  = 1e0   =   -"
+  ,"lit-  = 1e0   =   -"
+  ,"gra-  = 1e0   =   -"
+  ,"can-  = 1e0   =   -"
+  ,"amp-  = 1e0   =   -"
+  ,"mol-  = 1e0   =   -"
+  ,"kel-  = 1e0   =   -"
+  ,"sec-  = 1e0   =   -"
+  ]
+
+-- | 'MetricBaseUnit' provides an enumeration for the base metric unit types
+data MetricBaseUnit
+  = Meter
+  | Gram 
+  | Second
+  | Kelvin
+  | Liter
+  | Mole
+  | Ampere
+  | Candela
+  deriving (Show, Eq)
+
+-- | 'Unit' provides a wrapper for base units to include an appropriate prefix
+-- for example "milli" in front of a Meter
+data Unit = M Prefix MetricBaseUnit
+
+-- | 'Measurement' Wraps a double value to be associated with a unit
+data Measurement = MetricMeasurement Double Unit
+
+-- | Function grabs the prefix of an input string intended to be of the form
+-- <Double> <currentunit> to <desiredunit>. Function pulls out the prefix of <desiredunit>
+getPrefix :: 
+  String {- ^ input string -} ->
+  Either PrefixError Prefix {- ^ prefix associated with input -}
+getPrefix str = case parsePrefix $ take 3 $ reverse $ takeWhile (/= ' ') str of 
+                Left msg -> Left msg
+                Right (a, b) -> Right a
+
+strToMeasurement :: String -> Measurement
+strToMeasurement str = MetricMeasurement (parseValue str) (parseUnit str)
+
+-- | Used for conversion string input from user intended to be of the form
+-- <Double> <currentunit> to <desiredunit>. Function pulls out the <currentunit>
+-- and puts it in the Unit data constructor.
+parseUnit :: String -> Unit
+parseUnit str = M prefix (parseBase str)
+    where prefix =  case parsePrefix $ getPrefixLine (take 3 $ dropWhile (`notElem` ['a'..'z']) str) definitions of
+                            Left msg -> msg
+                            Right (a, b) -> a
+
+-- | Function looks at an input string and matches a metric base unit to return
+parseBase :: String -> MetricBaseUnit
+parseBase str
+    | "meter" `isInfixOf` str   = Meter
+    | "gram" `isInfixOf` str    = Gram
+    | "ampere" `isInfixOf` str  = Ampere
+    | "candela" `isInfixOf` str = Candela
+    | "liter" `isInfixOf` str   = Liter
+    | "mole" `isInfixOf` str    = Mole
+    | "kelvin" `isInfixOf` str  = Kelvin
+    | "second" `isInfixOf` str  = Second
+
+-- | Used for conversion string input from user intended to be of the form
+-- <Double> <currentunit> to <desiredunit>. Function pulls out the <Double> value
+-- by dropping anything that isn't a digit, then taking the digits, then reading them
+-- as a Double
+parseValue :: String -> Double
+parseValue = read . takeWhile isDigit . dropWhile (not . isDigit)
+
+-- | Takes a prefix string and the definitions list and parses through trying to match
+-- a definition and returning a single space as the prefix if none could be found
+getPrefixLine :: Prefix -> [String] -> String
+getPrefixLine str []      = " " -- if it is not found then it is the base unit with no prefix
+getPrefixLine str (x:xs)
+    | str `isPrefixOf` x  = x
+    | otherwise           = getPrefixLine str xs
+
+-- | parseAbbr takes a definitions string where the end of the string holds the unit
+-- abbreviation. It reverses the string to get to the end and parses until it hits
+-- the first space and returns the abbreviation
+parseAbbr ::
+  String {- ^ from definitions list that has had prefix dropped -} ->
+  String {- ^ as defined in definitions list -}
+parseAbbr str = drop 1 $ takeWhile (/= ' ') (reverse str)
+
+-- | Function parses a string and returns the Prefix and Prefix Abbr associated if there
+-- are defintions in the definitions string list
+parsePrefix :: 
+  String {- ^ Representing a unit definition -} ->
+  Either String (String, String)
+parsePrefix str
+    | str `isPrefixOf` x  = Right (takeWhile (/= '-') x, parseAbbr (drop (length str) x))
+    | otherwise           = Left "Prefix could not be found"
+        where
+        x = getPrefixLine str definitions
+
+
+-- | Helper function to print a units prefix and base unit
+unitStr :: Unit -> String
+unitStr u@(M prefix base) = prefix ++ lowerUnit u
+
+-- | Lowers the first character of an enum type of Unit converted to a string.
+-- Therefore Meter -> meter
+lowerUnit :: Unit -> String
+lowerUnit (M prefix base) = map toLower (show base)
+
+-- | Helper function to print the abbreviation of a Unit
+unitAbbr :: Unit -> String
+unitAbbr u@(M prefix base) = case (parsePrefix prefix) of
+                             Left msg -> msg
+                             Right (a, b) -> b ++ [head (lowerUnit u)]
+
+-- | Helper function to print the prefix of a unit
+unitPrefix :: Unit -> Prefix
+unitPrefix (M prefix base) = prefix
+
+-- | Helper function to print the base of a unit
+unitBase :: Unit -> MetricBaseUnit
+unitBase (M prefix base) = base
+
+-- | Parses the power from the defintions list to be used in conversions
+parsePower :: 
+  String {- ^ Unit Prefix to find the definition -} ->
+  String {- ^ The base 10 power of the prefix -}
+parsePower prefix = reverse $ dropWhile (== ' ') (reverse $ take 5 $ dropWhile (/= '1') line)
+    where line = (getPrefixLine prefix definitions)
+
+-- | Converts a given measurement to a new scale in the current measurement system
+-- given the prefix you want to convert it to
+convert :: Measurement -> Prefix -> Measurement
+convert (MetricMeasurement x unit) prefix 
+    = MetricMeasurement (x * (read (parsePower $ unitPrefix unit) :: Double) / (read (parsePower prefix) :: Double)) (M prefix (unitBase unit))
+
+-- | Gives the string form of a measurement with its unit abbreviation
+reportMeasurement :: Measurement -> String
+reportMeasurement (MetricMeasurement x unit) 
+    = show x ++ unitAbbr unit
+
+-- | Gives the full string form of a measurement with its full prefix and base name
+-- Sort of like a verbose -v option
+reportMeasurement' :: Measurement -> String
+reportMeasurement' (MetricMeasurement x unit)
+    = show x ++ unitStr unit
---
+++ Ryrc.hs
@@ -0,0 +1,110 @@
+import Data.List
+import Network
+import System.IO
+import System.Exit
+import System.Time
+import Control.Arrow
+import Control.Monad.Reader
+import Control.Exception
+import Text.Printf
+import Data.Units
+
+server = "irc.freenode.org"
+port   = 6667
+chan   = "#whatup"
+nick   = "ryrc"
+ 
+-- The 'Net' monad, a wrapper over IO, carrying the bot's immutable state.
+type Net = ReaderT Bot IO
+data Bot = Bot { socket :: Handle, starttime :: ClockTime }
+ 
+-- Set up actions to run on start and end, and run the main loop
+main :: IO ()
+main = bracket connect disconnect loop
+  where
+    disconnect = hClose . socket
+    loop st    = runReaderT run st
+ 
+-- Connect to the server and return the initial bot state
+connect :: IO Bot
+connect = notify $ do
+    t <- getClockTime
+    h <- connectTo server (PortNumber (fromIntegral port))
+    hSetBuffering h NoBuffering
+    return (Bot h t)
+  where
+    notify a = bracket_
+        (printf "Connecting to %s ... " server >> hFlush stdout)
+        (putStrLn "done.")
+        a
+ 
+-- Join a channel, and start processing commands
+run :: Net ()
+run = do
+    write "NICK" nick
+    write "USER" (nick++" 0 * :ryrc bot")
+    write "JOIN" chan
+    asks socket >>= listen
+ 
+-- Process each line from the server
+listen :: Handle -> Net ()
+listen h = forever $ do
+    s <- init `fmap` io (hGetLine h)
+    io (putStrLn s)
+    if ping s then pong s else eval (clean s)
+  where
+    forever a = a >> forever a
+    clean     = drop 1 . dropWhile (/= ':') . drop 1
+    ping x    = "PING :" `isPrefixOf` x
+    pong x    = write "PONG" (':' : drop 6 x)
+ 
+-- Dispatch a command
+eval :: String -> Net ()
+eval     "!quit"                    = write "QUIT" ":Exiting" >> io (exitWith ExitSuccess)
+eval x | "!id " `isPrefixOf` x      = privmsg (drop 4 x)
+eval     "!uptime"                  = uptime >>= privmsg
+eval x | "!convert -v" `isPrefixOf` x = privmsg (conv' (drop 11 x))
+eval x | "!convert " `isPrefixOf` x = privmsg (conv (drop 9 x))
+eval     _                          = return () -- ignore everything else
+ 
+-- Send a privmsg to the current chan + server
+privmsg :: String -> Net ()
+privmsg s = write "PRIVMSG" (chan ++ " :" ++ s)
+ 
+-- Send a message out to the server we're currently connected to
+write :: String -> String -> Net ()
+write s t = do
+    h <- asks socket
+    io $ hPrintf h "%s %s\r\n" s t
+    io $ printf    "> %s %s\n" s t
+ 
+io :: IO a -> Net a
+io = liftIO
+
+conv :: String -> String
+conv x =  case getPrefix (reverse x) of
+          Left msg -> msg
+          Right prefix -> reportMeasurement (convert (strToMeasurement x) prefix)
+
+conv' :: String -> String
+conv' x =  case getPrefix (reverse x) of
+          Left msg -> msg
+          Right prefix -> reportMeasurement' (convert (strToMeasurement x) prefix)
+
+-- Provide binding for uptime
+uptime :: Net String
+uptime = do
+    now <- io getClockTime
+    zero <- asks starttime
+    return . pretty $ diffClockTimes now zero
+
+-- taken from haskell.org on to pretty print time values
+pretty :: TimeDiff -> String
+pretty td =
+    unwords $ map (uncurry (++) . first show) $
+    if null diffs then [(0,"s")] else diffs
+    where merge (tot,acc) (sec,typ) = let (sec',tot') = divMod tot sec 
+                                      in (tot',(sec',typ):acc)
+          metrics = [(86400,"d"),(3600,"h"),(60,"m"),(1,"s")]
+          diffs = filter ((/= 0) . fst) $ reverse $ snd $
+                  foldl' merge (tdSec td,[]) metrics
